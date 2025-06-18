6ab340994bf9d7a1964b657789d3cd681ae574db
Lutow <118168487+Lutow@users.noreply.github.com>
2025-06-18 19:20:41 +0200

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const msg = ref('Loading...')
+
+onMounted(async () => {
+  const res = await fetch('http://localhost:8000/api/hello')
+  const data = await res.json()
+  msg.value = data.message
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
 
